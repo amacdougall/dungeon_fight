32f2dc4ca711750aa236ef692cc737a0c73a5fec
Alan MacDougall <smoke@alanmacdougall.com>
2011-07-09 23:49:57 -0400

    Moved from JSON to YAML; improved output during explore test.

--- test.py
+++ test.py
@@ -1,23 +1,46 @@
 from input_reader import InputReader
-from world.rooms import Map, Room
-import json
+from explore.rooms import Map, Room
+import yaml
+import textwrap
 
 reader = InputReader()
 
-map_data = json.load(open("test_map.json"))
+map_data = yaml.load(open("test_map.yaml"))
 
 test_map = Map()
 
-for room_data in map_data:
+for room_data in map_data["rooms"]:
     room = Room(name=room_data["name"],
                 brief=room_data["brief"],
                 verbose=room_data["verbose"])
     room.exits = room_data["exits"]
     test_map.rooms[room.name] = room
 
+test_map.location = test_map.rooms[map_data["starting_room"]]
+
 # game loop
-while command != "quit":
+here = test_map.location
+command = None
+
+# print startup messages
+print "Welcome to Dungeon Fight!"
+print
+print here.name
+print "\n".join(textwrap.wrap(here.verbose))
+print "Exits: %s" % ", ".join(here.exits.keys())
+
+# TODO: this, only better
+while not command in ["quit", "exit", "q"]:
     command = reader.read()[0]
+    if command in ["quit", "exit", "q"]:
+        break
+
+    if command == "l":
+        print here.name
+        print "\n".join(textwrap.wrap(here.verbose))
+        print "Exits: %s" % ", ".join(here.exits.keys())
+        continue
+
     here = test_map.move(command)
     if here is None:
         here = test_map.location
@@ -25,3 +48,6 @@ while command != "quit":
     else:
         print here.name
         print here.brief
+        print "Exits: %s" % ", ".join(here.exits.keys())
+
+print "bye"
